af42f1235f71f8be9a17f1ad5b492c71e6318ef6
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-03-07 09:42:36 +0300

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -7,7 +7,7 @@ with st.expander("ℹ️ - About this app", expanded=True):
     st.write(
         """     
 -   This app performs named entity recognition for medical entities. 
--   myDemo model was developed from dslim/distilbert-NER (a general NER model with 66M parameters) in HuggingFace, and fine-tuned on singh-aditya/MACCROBAT_biomedical_ner (a dataset annotated with medical entity labels in 41 categories). 
--   The model uses the default pretrained tokenizer in dslim/distilbert-NER.
+-   myDemo model was developed from dslim/distilbert-NER (a general NER model with 66M parameters) in HuggingFace, and fine-tuned on singh-aditya/MACCROBAT_biomedical_ner (a dataset annotated with medical entity labels in 41 categories). It uses the default pretrained tokenizer in dslim/distilbert-NER. 
+-   For comparison the output of blaze999/Medical-NER model is displayed in the right column. 
        """
     )
